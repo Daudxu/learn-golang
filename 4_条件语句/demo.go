6e4ddf7c05a64de847e77a2fba01473a1283a746
xudan <xudan@apowo.com>
2022-07-19 17:18:41 +0800

    条件判断

--- 4_条件语句/demo.go
+++ 4_条件语句/demo.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
-func convertToBin() string {
-	for {
-		fmt.Println("abc")
+func demo1() {
+	const fileName = "abc.txt"
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Printf("%s\n", contents)
 	}
 }
 
+func demo2(score int) string{
+	g :=""
+	switch {
+	case score < 60:
+    	g = "F"
+	default: //default case
+     	panic(fmt.Sprintf("Wrong score: %d", score))
+	}
+	return g
+}
+
 func main() {
-	convertToBin()
-	fmt.Println((123))
+	demo1()
+	fmt.Println(demo2(61))
+	fmt.Println(123)
 }
