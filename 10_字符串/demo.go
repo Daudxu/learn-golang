4798b5879451f10b81f5111a94107b5ab29105bf
xudan <xudan@apowo.com>
2022-07-25 17:56:19 +0800

    fix update

--- 10_字符串/demo.go
+++ 10_字符串/demo.go
@@ -1,73 +1,36 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"fmt"
-	"log"
-	"math/big"
-	"strings"
-	"time"
 
-	"token-contract/mytoken"
-
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/nanmu42/etherscan-api"
 )
 
-const key = `
-{
-  "address": "1a9ec3b0b807464e6d3398a59d6b0a369bf422fa",
-  "crypto": {
-    "cipher": "aes-128-ctr",
-    "ciphertext": "a471054846fb03e3e271339204420806334d1f09d6da40605a1a152e0d8e35f3",
-    "cipherparams": {
-      "iv": "44c5095dc698392c55a65aae46e0b5d9"
-    },
-    "kdf": "scrypt",
-    "kdfparams": {
-      "dklen": 32,
-      "n": 262144,
-      "p": 1,
-      "r": 8,
-      "salt": "e0a5fbaecaa3e75e20bccf61ee175141f3597d3b1bae6a28fe09f3507e63545e"
-    },
-    "mac": "cb3f62975cf6e7dfb454c2973bdd4a59f87262956d5534cdc87fb35703364043"
-  },
-  "id": "e08301fb-a263-4643-9c2b-d28959f66d6a",
-  "version": 3
-}
-`
-
 func main() {
-    conn, err := ethclient.Dial("\\\\.\\pipe\\geth.ipc")
-    if err != nil {
-        log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-    }
-    auth, err := bind.NewTransactor(strings.NewReader(key), "123")
-    if err != nil {
-        log.Fatalf("Failed to create authorized transactor: %v", err)
-    }
-    address, tx, token, err := mytoken.DeployMyToken(auth, conn, big.NewInt(9651), "Contracts in Go!!!", 0, "Go!")
-    if err != nil {
-        log.Fatalf("Failed to deploy new token contract: %v", err)
-    }
-    fmt.Printf("Contract pending deploy: 0x%x\n", address)
-    fmt.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
-    startTime := time.Now()
-    fmt.Printf("TX start @:%s", time.Now())
-    ctx := context.Background()
-    addressAfterMined, err := bind.WaitDeployed(ctx, conn, tx)
-    if err != nil {
-        log.Fatalf("failed to deploy contact when mining :%v", err)
-    }
-    fmt.Printf("tx mining take time:%s\n", time.Now().Sub(startTime))
-    if bytes.Compare(address.Bytes(), addressAfterMined.Bytes()) != 0 {
-        log.Fatalf("mined address :%s,before mined address:%s", addressAfterMined, address)
-    }
-    name, err := token.Name(&bind.CallOpts{Pending: true})
-    if err != nil {
-        log.Fatalf("Failed to retrieve pending name: %v", err)
-    }
-    fmt.Println("Pending name:", name)
+	// 创建连接指定网络的客户端
+	client := etherscan.New(etherscan.Rinkby, "[W6127C4ADI6M4RBYV5NVXA4VJPTEJH5NYV]")
+	
+	// 或者，如果你要调用的是EtherScan家族的BscScan：
+	//
+	// client := etherscan.NewCustomized(etherscan.Customization{
+	// Timeout:       15 * time.Second,
+	// Key:           "You key here",
+	// BaseURL:       "https://api.bscscan.com/api?",
+	// Verbose:       false,
+	// })	
+	
+
+	// 查询账户以太坊余额
+	balance, err := client.AccountBalance("0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166")
+	if err != nil {
+		panic(err)
+	}
+	// 余额以 *big.Int 的类型呈现，单位为 wei
+	fmt.Println(balance.Int())
+
+	// // 查询token余额
+	// tokenBalance, err := client.TokenBalance("contractAddress", "holderAddress")
+
+	// // 查询出入指定地址的ERC20转账列表
+	// transfers, err := client.ERC20Transfers("contractAddress", "address", startBlock, endBlock, page, offset)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go
 go 1.18
 
 require (
+	github.com/nanmu42/etherscan-api v1.8.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	golang.org/x/tools v0.1.11 // indirect
